9e3a8e827ef1e10955824ce7162d1d7110d4bab5
João Santos <johnnie-jr@hotmail.com>
2021-07-06 18:21:52 -0300

    Commit pratica 3 pull request

--- Git-Aula-2021-1/src/HelloWorld.java
+++ Git-Aula-2021-1/src/HelloWorld.java
@@ -20,6 +20,7 @@ public class HelloWorld {
 		System.out.println("Criando um novo commit para a pr�tica 3 - Guilherme Henrique");
 		System.out.println("Criando um novo commit para a pr�tica 3 - Guilherme Henrique - Pull Request");
 		System.out.println("Criando um novo commit para a pr�tica 3 - João Santos");
+		System.out.println("Criando um novo commit para a pr�tica 3 - João Santos - Pull Request");
 		
         System.out.println("Commit Eclipse + BASH !!!");
 		System.out.println("Criando um novo commit - Natasha Barbachan");
